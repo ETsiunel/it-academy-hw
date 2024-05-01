dc4b94fd6ce578587ea2c90f192444e8b350ff46
ETsiunel <y.tsyunel@gmail.com>
2024-05-01 22:14:15 +0300

    fix main file_1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """Module providing a function printing text."""
 
 def main():
-        """Function printing python text."""
     print("Hello world!")
+
+    
 if __name__ == "__main__":
     main()
