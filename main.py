532d49bb412b49141c0d190935dbee5769615919
ETsiunel <y.tsyunel@gmail.com>
2024-05-07 21:36:40 +0300

    hw_5_strings_fix_1

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 """Module providing a function printing text."""
 
-
 def main():
     """Function printing text."""
-
-
     print("Hello world!")
-
 
 if __name__ == "__main__":
     main()
@@ -14,17 +10,21 @@ if __name__ == "__main__":
 # Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
 A = "www.my_site.com#about"
 print(A.replace("#", "/"))
+
 # Напишите программу, которая добавляет ‘ing’ к словам
 B = "word"
 print(B + "ing")
+
 # В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
 C = "Ivanou Ivan"
 C1 = C.split()
 print(C1[1] + " " + C1[0])
+
 # Напишите программу которая удаляет пробел в начале, в конце строки
 # // написала для случая удаления и в начале, и в конце
 D = " Тестовая строка с пробелами "
 print(D.strip())
+
 # Имена собственные всегда начинаются с заглавной буквы,
 # за которой следуют строчные буквы.
 # Исправьте данное имя собственное так,
