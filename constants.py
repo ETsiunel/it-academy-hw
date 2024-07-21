60d0a7439c9a6021cc8f1cfb88b195e46d0391fa
ETsiunel <y.tsyunel@gmail.com>
2024-07-21 14:06:46 +0300

    homework_21_remove_reports_add_conftest

---
+++ constants.py
@@ -0,0 +1,8 @@
+LOG_LEVELS = {
+    "CRITICAL": 50,
+    "ERROR": 40,
+    "WARNING": 30,
+    "INFO": 20,
+    "DEBUG": 10,
+    "NOTSET": 0
+}
---
+++ hw_21/tests/conftest.py
@@ -0,0 +1,28 @@
+from hw_21.tests.my_logger import logger as log
+from constants import LOG_LEVELS
+
+
+def pytest_addoption(parser):
+    """Function adds a command-line option to set the HTML log level"""
+    parser.addoption(
+        "--html-log-level",
+        action="store",
+        default="DEBUG",
+        help="Set the logging level for html report"
+    )
+
+
+def pytest_configure(config):
+    """Function configures logging based on the command-line option"""
+    log_level = config.getoption("--html-log-level").upper()
+    if log_level_numeric := LOG_LEVELS.get(log_level):
+        log.setLevel(log_level_numeric)
+
+
+# To run:
+#   pytest test_library.py --log-cli-level=ERROR --html=report_bank.html
+#       - вывод уровня ERROR в консоль, в html-отчете уровни по дефолту
+#   pytest test_library.py --log-level=ERROR --html=report_bank.html
+#       - вывод уровня ERROR в html-отчет, в консоли уровни по дефолту
+#   pytest test_bank.py --log-cli-level=ERROR --log-level=ERROR --html=report_bank.html
+#       - вывод уровня ERROR везде
