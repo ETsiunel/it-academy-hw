ec723fb5286ee7a68c185167b654c264c30cb9cc
ETsiunel <y.tsyunel@gmail.com>
2024-07-24 23:52:15 +0300

    homework_21_remove_pytest.ini_change_conftest

--- hw_21/tests/conftest.py
+++ hw_21/tests/conftest.py
@@ -1,30 +1,19 @@
 """Conftest"""
 
-from hw_21.tests.my_logger import logger as log
-from constants import LOG_LEVELS
 
-
-def pytest_addoption(parser):
-    """Function adds a command-line option to set the HTML log level"""
-    parser.addoption(
-        "--html-log-level",
-        action="store",
-        default="DEBUG",
-        help="Set the logging level for html report"
-    )
-
-
-def pytest_configure(config):
-    """Function configures logging based on the command-line option"""
-    log_level = config.getoption("--html-log-level").upper()
-    if log_level_numeric := LOG_LEVELS.get(log_level):
-        log.setLevel(log_level_numeric)
+def pytest_html_report_title(report):
+    report.title = "Pytest HTML Report Library"
 
 
 # To run:
-#   pytest test_library.py --log-cli-level=ERROR --html=report_library.html
-#       - вывод уровня ERROR и выше в консоль, в html-отчете уровни по дефолту
-#   pytest test_library.py --log-level=ERROR --html=report_library.html
-#       - вывод уровня ERROR и выше в html-отчет, в консоли уровни по дефолту
-#   pytest test_bank.py --html-log-level=ERROR --html=report_bank.html
-#       - вывод уровня ERROR и выше везде
+#   pytest test_library.py --log-cli-level=ERROR
+#       --html=report.html --self-contained-html
+#           - вывод уровня ERROR и выше в консоль,
+#           - в html-отчете уровни по дефолту
+#   pytest test_library.py --log-level=ERROR
+#       --html=report.html --self-contained-html
+#           - вывод уровня ERROR и выше в html-отчет
+#           - в консоли лог не пишет
+#   pytest test_library.py --log-cli-level=ERROR
+#       --html=report.html --self-contained-html --log-level=ERROR
+#           - вывод уровня ERROR и выше везде
